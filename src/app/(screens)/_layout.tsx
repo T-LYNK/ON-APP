0bfc0811d13db88b43d748372ea6be962744e2db
Thyago Santos <thyagomatheus15@gmail.com>
2024-06-03 18:11:58 -0300

    Add files via upload

--- src/app/(screens)/_layout.tsx
+++ src/app/(screens)/_layout.tsx
@@ -1,22 +1,23 @@
 import "../../global.css";
 import React from "react";
+import { Stack } from "expo-router";
 import { View, Text } from "react-native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from "@react-navigation/native";
-import { ScreenStack } from "react-native-screens";
 import Auth from "./auth";
 import Register from "./register";
 import Request from "./request/category";
+import Seach from "./request/seach";
+import editprofile from "./settings/editprofile";
 
-const Stack = createStackNavigator();
 
 export default function Layout() {
   return (
-        <Stack.Navigator initialRouteName="auth" screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="auth" options={{ headerShown: false }} component={Auth} />
-          <Stack.Screen name="register" options={{ headerShown: false }} component={Register} />
-          <Stack.Screen name="request" options={{ headerShown: false }} component={Request} />
-        </Stack.Navigator>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="auth" options={{ headerShown: false }} />
+          <Stack.Screen name="register" options={{ headerShown: false }}  />
+          <Stack.Screen name="request" options={{ headerShown: false }} />
+          <Stack.Screen name="seach" options={{ headerShown: false }}  />
+          <Stack.Screen name="editprofile" options={{ headerShown: false }} />
+        </Stack>
   );
 };
 
--- src/app/(screens)/register.tsx
+++ src/app/(screens)/register.tsx
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, View, SafeAreaView, Image, KeyboardAvoidingView, TextInput, Pressable, } from "react-native";
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useRouter, useNavigation } from "expo-router";
 
@@ -10,10 +10,23 @@ const Auth = () => {
     const [password, setPassword] = useState("");
     const router = useRouter();
     const navigation = useNavigation();
+
+    const handleSubmit = async () => {
+      if ( name === '' || email === '' || password === '' ) {
+        alert("All fields are required");
+        return;
+      }
+      const resp = await axios.post("http://localhost:5000/auth/add", { name, email, password } );
+       if(resp.data.error)
+        alert(resp.data.error)
+        else
+      alert("Sign Up Successful");
+    };
+
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: "white", alignItems: "center"}} >
             <View>
-                <Image style={{ width : 150, height: 100, resizeMode: "contain"}}
+                <Image style={{ marginTop: 20, width : 150, height: 100, resizeMode: "contain"}}
                 source={{uri: "https://on-panel.vercel.app/images/logotipo-black.png",}}/>
             </View>
             <KeyboardAvoidingView>
@@ -25,29 +38,30 @@ const Auth = () => {
                 <View style={{ marginTop: 70 }} >
                     <View style={{ flexDirection: "row", alignItems: "center", gap: 5, backgroundColor: "#E0E0E0", paddingVertical: 5, borderRadius: 50, marginTop: 30, }} >
                     <TextInput style={{ borderRadius:50, fontWeight: "bold", textAlign: "center", color: "gray", width: 300, fontSize: name ? 30 : 30, }}
-                    placeholder="Name" 
+                    placeholder="Name" value={name} onChangeText={text => setName(text)}
                     /> 
                     </View>
                     
                     <View style={{ flexDirection: "row", alignItems: "center", gap: 5, backgroundColor: "#E0E0E0", paddingVertical: 5, borderRadius: 50, marginTop: 30, }} >
                     <TextInput style={{ borderRadius:50, fontWeight: "bold", textAlign: "center", color: "gray", width: 300, fontSize: email ? 30 : 30, }}
-                    placeholder="Email" 
+                    placeholder="Email" value={email} onChangeText={text => setEmail(text)}
                     /> 
                     </View>
                 <View style={{ marginTop: 0 }} >
                 <View style={{ flexDirection: "row", alignItems: "center", gap: 5, backgroundColor: "#E0E0E0", paddingVertical: 5, borderRadius: 50, marginTop: 30, }} >
-                <TextInput textContentType="password" secureTextEntry={true} style={{ borderRadius:50, fontWeight: "bold", textAlign: "center", color: "gray", width: 300, fontSize: password ? 30 : 30, }} placeholder="Password"/>
+                <TextInput textContentType="password" secureTextEntry={true} style={{ borderRadius:50, fontWeight: "bold", textAlign: "center", color: "gray", width: 300, fontSize: password ? 30 : 30, }} placeholder="Password" value={password} onChangeText={text => setPassword(text)}/>
                 </View>
                 </View>
 
                 </View>
               <View style={{ marginTop: 80 }} >
-                <Pressable onPress={() => navigation.goBack()} style={{ width: 200, backgroundColor: "blue", borderRadius: 50, marginLeft: "auto", marginRight: "auto", padding: 15, }} > 
+                <Pressable onPress={handleSubmit} style={{ width: 200, backgroundColor: "blue", borderRadius: 50, marginLeft: "auto", marginRight: "auto", padding: 15, }} > 
                 <Text style={{ textAlign: "center", color: "white", fontSize: 16, fontWeight: "bold", }} >
                 Register
                 </Text>
                     </Pressable>
                     <View>
+                      <Text>{JSON.stringify({name, email})}</Text>
                   <Text onPress={() => navigation.navigate({ name : 'auth'})} style={{ marginTop: 30, fontSize: 18, textAlign: "center" }}>Already have an Account ? Login</Text>
                 </View>
               </View>
