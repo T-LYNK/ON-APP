e3f205a7cfd388e2c888b85a58b7de49839349da
Thyago Santos <thyagomatheus15@gmail.com>
2024-10-19 20:02:15 -0300

    Update CustomDrawerContent.tsx

--- src/components/CustomDrawerContent.tsx
+++ src/components/CustomDrawerContent.tsx
@@ -11,14 +11,13 @@ export default function CustomDrawerContent(props) {
     const { top, bottom } = useSafeAreaInsets();
     return (
         <View style={{ flex: 1 }}>
-        <DrawerContentScrollView  {...props} scrollEnabled={false} >
+        <DrawerContentScrollView  scrollEnabled={false} >
             <View style={{ padding: 10, justifyContent: 'center', alignItems: 'center', alignSelf: 'center' }}>
             <Image source={{ uri: 'https://on-application.vercel.app/images/default-blue.png' }} style={{ paddingTop: 50, justifyContent: 'center', alignItems: 'center', borderRadius: 100, width: 80, height: 80,  }} />
             <Text style={{ padding: 0,  alignSelf: 'center', fontWeight: 'bold', fontSize: 25, paddingTop: 0, color: 'black' }}>
             UserName
             </Text>
             </View>
-           <DrawerItemList {...props} /> 
         </DrawerContentScrollView>
         <View style={{ borderTopColor: '#dde3fe', borderTopWidth: 1, padding: 20, paddingBottom: 20 + bottom }}>
             <Link href={"/(screens)/auth"} style={{ fontSize:25, fontWeight:"bold" }}><Ionicons name="power" size={25} /> Logout </Link>
